b9d019b2f852131ecf3c38f7011be5414957b55e
Gyanendra Baghel <gyanendrabaghel633@gmail.com>
2024-10-28 23:51:37 +0530

    backend changes

--- context/socket.js
+++ context/socket.js
@@ -4,24 +4,24 @@ import { io } from "socket.io-client";
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-    const socket = useContext(SocketContext)
-    return socket
-}
+  const socket = useContext(SocketContext);
+  return socket;
+};
 
 export const SocketProvider = (props) => {
   const { children } = props;
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const connection = io();
-    console.log("socket connection", connection)
+    const connection = io("https://meet-backend-s9s9.onrender.com/");
+    console.log("socket connection", connection);
     setSocket(connection);
   }, []);
 
-  socket?.on('connect_error', async (err) => {
-    console.log("Error establishing socket", err)
-    await fetch('/api/socket')
-  })
+  socket?.on("connect_error", async (err) => {
+    console.log("Error establishing socket", err);
+    await fetch("https://meet-backend-s9s9.onrender.com/socket");
+  });
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
